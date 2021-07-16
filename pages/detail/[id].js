54ed6542984a0f56ca293a9e2cc88dd5431bebd0
SophiaLangheld <gaoxuefei55@gmail.com>
2021-07-16 12:43:03 +0200

    add useRouter

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,8 +1,10 @@
 import React from "react";
-import {useRouter} from "next/router";
+import {useRouter} from "next/router"; //动态路由
 
+//http://localhost:3000/detail/3 这样给URL不报错
 export default function Detail(){
     const router = useRouter()
+    //console.log(router)
     const {id} = router.query;
     console.log(id)
     return 'detail'
